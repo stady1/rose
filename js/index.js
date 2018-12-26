b16fd4566183770f2224dbf58e9bc2123b3b902b
Ann <ganna.sapii@gmail.com>
2018-12-26 10:52:51 +0000

    nn

--- js/index.js
+++ js/index.js
@@ -1,34 +1,11 @@
-  // ФОРМА
-  // document.querySelector('.page-loaded').innerText =
-  //     (new Date()).toLocaleTimeString();
-  
-  
-  // document.querySelector('.get-html-ajax')
-  //     .addEventListener('click', getHtmlAjax);
-  
-  // function getHtmlAjax() {
-  //     const xhr = new XMLHttpRequest();
-  //     xhr.onreadystatechange = function() {
-  //         if (xhr.readyState === 4 && xhr.status === 200) {
-  //             document.querySelector('.html-placeholder').innerHTML = xhr.responseText;
-  //         }
-  //     }
-  
-  //     document.querySelector('.login-form input[type=submit]')
-  //         .addEventListener('click', submitForm);
-  
-  //     function submitForm(ev) {
-  //         if (document.querySelector('.login-form').checkValidity()) {
-  //             ev.preventDefault();
-  //             const xhr = new XMLHttpRequest();
-  //             xhr.onreadystatechange = function() {
-  //                 if (xhr.readyState === 4 && xhr.status === 200) {
-  //                     document.querySelector('.message').innerHTML = xhr.responseText;
-  //                 }
-  //             }
-  //             xhr.open('POST', 'form.php', true);
-  //             const formData = new FormData(document.querySelector('.login-form'));
-  //             xhr.send(formData);
-  //         }
-  //     }
-  // }
+  $(function() {
+    $('.smooth').on('click', function(event) {
+        var target = $(this.getAttribute('href'));
+        if (target.length) {
+            event.preventDefault();
+            $('html, body').stop().animate({
+                scrollTop: target.offset().top
+            }, 700);
+        }
+    });
+});
